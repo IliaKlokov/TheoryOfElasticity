6bd1030e3a198a19b95a418144bf10a91c2ef266
olegknjazev <olegknjazev@ro.ru>
2024-01-11 23:04:17 +0300

    Update Material_body.py

--- models/Material_body.py
+++ models/Material_body.py
@@ -1,8 +1,8 @@
-class Material_body:
-    rx = None
-    ry = None
-    ang = None
-    def __init__(self, rx, ry, ang):
-        self.rx = rx
-        self.ry = ry
-        self.ang = ang
+class materialBody:
+    x1c = None
+    x2c = None
+    points = []
+    def __init__(self, x1c, x2c, points):
+        self.x1 = x1c
+        self.x2 = x2c
+        self.points = points
