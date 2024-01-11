8603d386786668e12af950419686ac681a01b063
olegknjazev <olegknjazev@ro.ru>
2024-01-11 20:55:11 +0300

    Update create_body.py

--- methods/create_body.py
+++ methods/create_body.py
@@ -1,4 +1,9 @@
-from models.Material_body import Material_body
-def create_body(rx,ry,ang):
-    mb=Material_body(rx, ry, ang)
+from models.Material_body import materialBody
+from models.Material_point import Material_point
+def create_body(xc,yc):
+    p = []
+    for i in range(-0.5,0.5,0.5):
+        for j in range(-0.5,0.,0.5):
+            p.append(Material_point(xc+j,yc+j))
+    mb = materialBody(xc, yc, p)
     return mb
