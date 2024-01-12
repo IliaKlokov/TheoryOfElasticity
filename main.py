efc0074f870d3e2c5723de3db9819fb3a383ed8c
olegknjazev <olegknjazev@ro.ru>
2024-01-12 16:02:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from methods.move_through_space import movet
 from methods.create_body import create_body
+from methods.plot_streamlines import plot_streamlines
 import matplotlib.pyplot as plt
 mbc, mbpx, mbpy = create_body(-2,-2,0,10)
 plt.plot(mbpx,mbpy)
 plt.show()
 movet(0.1,1,25,mbpx,mbpy)
-
+print(mbpx,mbpy)
+plot_streamlines(mbpx, mbpy[0], mbpy[-1], len(mbpx), 1)
