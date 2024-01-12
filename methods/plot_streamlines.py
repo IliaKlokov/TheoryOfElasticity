80bc10a28fc7536e512c27b9cb6fc94d7750c9b1
olegknjazev <olegknjazev@ro.ru>
2024-01-12 17:21:28 +0300

    Update plot_streamlines.py

--- methods/plot_streamlines.py
+++ methods/plot_streamlines.py
@@ -7,15 +7,21 @@ def streamlines(mb, y, time):
 
 def plot_streamlines(mb, y_min, y_max, y_num, time):
     y = np.linspace(y_min, y_max, y_num)
-    t = np.linspace(0.1,time,10)
+    t = np.linspace(0.1, time, 10)
+    a, b = 0, 0
     for j in range(len(t)):
         fig, ax = plt.subplots()
         for i in range(len(mb)):
             x = streamlines(mb[i], y, t[j])
-            plt.title('t = ' + str((j+1)*0.1))
+            plt.title('t = ' + str((j + 1) * 0.1))
             ax.plot(x, y, 'r-')
-        v1 = [-np.exp(t[j])*k for k in mb]
-        v2 = [np.log(t[j])*k for k in mb]
-        ax.quiver(mb, y, v1, v2)    
-        plt.show()
-        #plt.savefig(str((j+1)*0.1)+'.png', format='png')
+            if (min(x) < a): a = min(x)
+        x = np.linspace(a, max(mb), y_num//3)
+        y = np.linspace(y_min, y_max, y_num//3)
+        v1 = [-np.exp(t[j])*x[i] for i in range(len(x))]
+        v2 = [np.log(t[j])*y[i] for i in range(len(y))]
+        for i in range(len(x)):
+            for k in range(len(y)):
+                plt.quiver(x[i], y[k], v1[i], v2[k])
+        #plt.show()
+        plt.savefig(str((j+1)*0.1)+'.png', format='png')
