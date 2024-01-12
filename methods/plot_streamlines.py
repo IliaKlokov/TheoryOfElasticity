fd203e24c134c9ccbda80b34c8423eca4778cb5e
olegknjazev <olegknjazev@ro.ru>
2024-01-12 16:01:59 +0300

    Update plot_streamlines.py

--- methods/plot_streamlines.py
+++ methods/plot_streamlines.py
@@ -1,16 +1,21 @@
-import numpy as np
 import matplotlib.pyplot as plt
-import math
-def plot_streamlines(t,x1,x2):
-    X1, X2 = np.meshgrid(x1, x2)
+import numpy as np
 
-    V1 = -math.exp(t) * X1
-    V2 = math.log(t) * X2
+def streamlines(mb, y, time):
+    x = mb*np.fabs(y)**(-np.exp(time)/np.log(time))
+    return x
 
-    plt.streamplot(X1, X2, V1, V2, density=2)
-
-    plt.xlabel('x1')
-    plt.ylabel('x2')
-    plt.title('Линии тока для поля скоростей')
-    plt.grid()
-    plt.show()
+def plot_streamlines(mb, y_min, y_max, y_num, time):
+    y = np.linspace(y_min, y_max, y_num)
+    t = np.linspace(0.1,time,10)
+    for j in range(len(t)):
+        fig, ax = plt.subplots()
+        for i in range(len(mb)):
+            x = streamlines(mb[i], y, t[j])
+            plt.title('t = ' + str((j+1)*0.1))
+            ax.plot(x, y, 'r-')
+        v1 = [-np.exp(t[j])*k for k in mb]
+        v2 = [np.log(t[j])*k for k in mb]
+        ax.quiver(mb, y, v1, v2)    
+        plt.show()
+        #plt.savefig(str((j+1)*0.1)+'.png', format='png')
