6db183c379d25a31c0ee3e2f46b9053d4e4a6166
IliaKlokov <128388751+IliaKlokov@users.noreply.github.com>
2024-01-12 11:01:43 +0300

    task

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from methods.move_through_space import movet
 from methods.create_body import create_body
 import matplotlib.pyplot as plt
-mbc, mbpx, mbpy = create_body(1,1,0,25)
+mbc, mbpx, mbpy = create_body(-2,-2,0,10)
 plt.plot(mbpx,mbpy)
 plt.show()
-movet(1,1,25,mbpx,mbpy)
+movet(0.1,1,25,mbpx,mbpy)
 
--- methods/move_through_space.py
+++ methods/move_through_space.py
@@ -1,6 +1,9 @@
 from methods.move_body import move
 from methods.plot_trajectory import plottr
+import matplotlib.pyplot as plt
 def movet(t0, t, n, x, y):
     for i in range(len(x)):
         res=move(t0, t, n, x[i], y[i])
-        plottr(res.x_tr_points, res.y_tr_points)
+        #plottr(res.x_tr_points, res.y_tr_points)
+        plt.plot(res.x_tr_points, res.y_tr_points)
+    plt.show()
