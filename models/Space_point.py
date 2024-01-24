60ccd9a63fc4511e7d65b498578eb9557b88645d
olegknjazev <olegknjazev@ro.ru>
2024-01-24 21:32:54 +0300

    Update Space_point.py

--- models/Space_point.py
+++ models/Space_point.py
@@ -1,6 +1,6 @@
 class Space_point:
-    ra1 = None
-    ra2 = None
-    def __init__(self, ra1, ra2):
-        self.ra1 = ra1
-        self.ra2 = ra2
+    x1 = None
+    x2 = None
+    def __init__(self, x1, x2):
+        self.ra1 = x1
+        self.ra2 = x2
