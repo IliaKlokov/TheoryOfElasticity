95665a380df2738d78af2222fca1a3e0a7e8d999
olegknjazev <olegknjazev@ro.ru>
2024-01-25 20:07:48 +0300

    Update plot_streamlines.py

--- methods/plot_streamlines.py
+++ methods/plot_streamlines.py
@@ -9,7 +9,6 @@ def plot_streamlines(time, min, max):
     plt.streamplot(x, y, s.v1, s.v2, density=0.3, color='red')
     a = np.linspace(min, max, 4*(max-min)+1)
     x, y = np.meshgrid(a, a)
-    s = streamLine(time, x, y)
-    plt.quiver(x, y, s.v1, s.v2, scale=100, )
+    plt.quiver(x, y, s.v1, s.v2, scale=100)
     plt.title(f'time = {time}')
     plt.show()
