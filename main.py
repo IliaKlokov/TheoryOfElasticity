add36b686b17c6a564b276c26fc7a124f516b4b8
olegknjazev <olegknjazev@ro.ru>
2024-01-25 19:15:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from methods.plot_streamlines import plot_streamlines
 import matplotlib.pyplot as plt
 import numpy as np
 mbc, mbpx, mbpy = create_body(-2,-2,0,10)
-#plt.plot(mbpx,mbpy)
-#plt.show()
-#movet(0.1,1,25,mbpx,mbpy)
-#print(mbpx,mbpy)
+plt.plot(mbpx,mbpy)
+plt.show()
+movet(0.1,1,25,mbpx,mbpy)
+print(mbpx,mbpy)
 for t in np.arange(0.1, 1, 0.1):
     plot_streamlines(t, -3, 0)
